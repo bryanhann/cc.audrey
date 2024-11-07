cde5b589ed539e527dde70c520c4807543a01614
Bryan Hann <nobody@nowhere>
2024-11-07 13:50:26 -0330

    Modify hooks to respect new options:
    [cookiecutter.hook_bash]:
    
        Make the hooks pause before exiting to allow
        the user to run a bash shell first
    
    [cookiecutter.github_deploy]:
    
        Make [./hooks/pre_gen_project.py] respect this
        option by aborting if the the repo already exists
        on github.
    
        Make [./hooks/post_gen_project.py] respect this
        option by executing [__run_me__.sh], thus deploying
        the project to github.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,17 +1,61 @@
 #!/usr/bin/env python
+import sys
 import pathlib
 import subprocess
 
-if __name__ == '__main__':
+RUNME = './__run_me__.sh'
+REPO = "{{ cookiecutter.github_repo_name }}"
+MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
+module_name = '{{ cookiecutter.project_slug}}'
+HOOK_BASH =  "{{ cookiecutter.hook_bash }}".lower()
+DEPLOY = '{{ cookiecutter.github_deploy }}'.lower() == 'y'
+BAR='='*60
 
+def main():
+    print( f"{BAR}\nENTER post_gen_project\n{BAR}" )
+    process_authors()
+    process_cli()
+    process_open_source()
+    process_deploy_or_die()
+    die(0, "script ended normally")
+
+def process_deploy_or_die():
+    if DEPLOY:
+        code = subprocess.run( [ RUNME ] ).returncode
+        print( f"[{RUNME}] exited with exit code [{code}]" )
+        code == 0 or die(0, "script ended with bad initialization")
+
+def process_authors():
     if '{{ cookiecutter.create_author_file }}' != 'y':
         pathlib.Path('AUTHORS.rst').unlink()
         pathlib.Path('docs', 'authors.rst').unlink()
 
+def process_cli():
     if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
         pathlib.Path('src', '{{ cookiecutter.project_slug }}', 'cli.py').unlink()
 
+def process_open_source():
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         pathlib.Path('LICENSE').unlink()
 
-    subprocess.run( [ './__run_me__.sh' ] )
+
+
+def exists(name):
+     it = run( 'gh repo list --json name'.split(), capture_output=True)
+     names = [ item['name'] for item in json.loads(it.stdout) ]
+     return name in names
+
+def torf(prompt):
+    while True:
+        answer = input( f'{prompt} [y/n]?' ).lower()
+        if answer=='y': return True
+        if answer=='n': return False
+
+def die( code, msg='no message' ):
+    print(f"{BAR}\nEXIT pre_post_project\n[{code}]: {msg}\n{BAR}")
+    if HOOK_BASH == 'y' and torf( 'run bash' ): run( ['bash'] )
+    sys.exit(code)
+
+if __name__ == '__main__':
+    main()
+
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,13 +1,50 @@
 import re
 import sys
+from subprocess import run
+import json
+BAR='='*60
 
-
+REPO = "{{ cookiecutter.github_repo_name }}"
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
-
 module_name = '{{ cookiecutter.project_slug}}'
+HOOK_BASH =  "{{ cookiecutter.hook_bash }}".lower()
+DEPLOY = '{{ cookiecutter.github_deploy }}'.lower()
 
-if not re.match(MODULE_REGEX, module_name):
-    print('ERROR: The project slug (%s) is not a valid Python module name. '
-          'Please do not use a - and use _ instead' % module_name)
-    #Exit to cancel project
-    sys.exit(1)
+def main():
+    print( f"{BAR}\nENTER pre_gen_project\n{BAR}" )
+    print( f"HOOK_BASH: [{HOOK_BASH}]" )
+    verify_plug_or_die()
+    verify_deploy_die()
+    die(0)
+
+def verify_plug_or_die():
+    if not re.match(MODULE_REGEX, module_name):
+        print('ERROR: The project slug (%s) is not a valid Python module name. '
+              'Please do not use a - and use _ instead' % module_name)
+        die(1)
+
+def verify_deploy_die():
+    if DEPLOY == 'y':
+        print( 'deploying. Checking for existance of repo' )
+        exists( REPO ) and die( 101, 'repo already existss' )
+
+
+def exists(name):
+     it = run( 'gh repo list --json name'.split(), capture_output=True)
+     names = [ item['name'] for item in json.loads(it.stdout) ]
+     return name in names
+
+def torf(prompt):
+    while True:
+        answer = input( f'{prompt} [y/n]?' ).lower()
+        if answer=='y': return True
+        if answer=='n': return False
+
+def die( code, msg='no message' ):
+    print(f"{BAR}\nEXIT pre_gen_project\n[{code}]: {msg}\n{BAR}")
+    if HOOK_BASH == 'y' and torf( 'run bash' ): run( ['bash'] )
+    sys.exit(code)
+
+
+if __name__ == '__main__':
+    main()
